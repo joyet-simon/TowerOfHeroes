969a54815f4adf9fb74d6414ae08b62a99c7f867
joyet simon <43644110+joyet-simon@users.noreply.github.com>
2020-09-28 11:11:59 +0200

    [Angular] Display a List

--- Angular/src/app/heroes/heroes.component.ts
+++ Angular/src/app/heroes/heroes.component.ts
@@ -1,21 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from "../shared/hero";
+import { Hero } from '../shared/hero';
+import { HEROES } from '../shared/mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-
-  hero: Hero = {
-    id: 1,
-    name: "Cloud Strife"
-  };
-  
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
 
+  heroes = HEROES;
+
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
---
+++ Angular/src/app/shared/mock-heroes.ts
@@ -0,0 +1,17 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+    { id: 1, name: "Cloud Strife" },
+    { id: 2, name: "Aeris Gainsborough" },
+    { id: 3, name: "Tifa Lockhart" },
+    { id: 4, name: "Barret Wallace" },
+    { id: 5, name: "Nanaki" },
+    { id: 6, name: "Cid Highwind" },
+    { id: 7, name: "Cait Sith" },
+    { id: 8, name: "Vincent Valentine" },
+    { id: 9, name: "Yuffie Kisaragi" },
+    { id: 10, name: "Wedge" },
+    { id: 11, name: "Biggs" },
+    { id: 12, name: "Jessie" },
+];
+
